878d671a57147b2a0a41f9ef3fcbfd3acbc37b35
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-02 22:27:26 -0600

    commit tree 7294ee40dbeeb1fdd0ca5bb13a3886645f294018

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => setUrl(data.message));
+    }, [])
+
+    if (!url) return <p>Loading...</p>;
+    //console.log(url);
+
+    return (
+        <div>
+            <img src={url} alt="A Random Dog"/>
+        </div>
+    )
+};
+
+export default App;
